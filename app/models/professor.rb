9b5f0bc9db0341bc23b4831678e0b5f22f78120e
diego-asterisk <diegomanias@gmail.com>
2017-12-21 16:36:23 -0300

    barra de navegacion

---
+++ app/models/professor.rb
@@ -0,0 +1,6 @@
+class Professor < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :recoverable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :rememberable, :trackable, :validatable
+end
