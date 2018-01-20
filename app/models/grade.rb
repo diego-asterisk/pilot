fbb51c6def92d9ce3318e3c133b7226c12f53081
diego-asterisk <diegomanias@gmail.com>
2018-01-20 18:24:51 -0300

    Evitar el uso de return y de defined?

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -2,11 +2,26 @@ class Grade < ApplicationRecord
   validates :year, presence: true, numericality: { greater_than_or_equal_to: 2000 }, uniqueness: true
   
   def self.find_current(grade_id)
-    return grade_id if defined?(grade_id) and grade_id.present? and self.exists? grade_id
-    if self.exists?
-      return self.order('year DESC').first.id
+    if grade_id.nil?
+       ''
+    else
+      self.find_current_id(grade_id)
     end
-    return ''
+  end
+  def self.find_current_id(grade_id)
+    if self.exists? grade_id
+       grade_id
+    else
+       self.find_id_alterno()
+    end
+  end 
+  def self.find_id_alterno()
+    alternativo = self.order('year DESC').first
+    if alternativo.nil?
+       ''
+    else
+       alternativo.id
+    end
   end
 end
 
